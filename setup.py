85deaf2193d668c7615df86c92bb90abc123e9d7
Michael Fuerst <mail@michaelfuerst.de>
2020-10-19 22:04:37 +0200

    New release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '20201012'
+__version__ = '20201019'
 
 here = path.abspath(path.dirname(__file__))
 
