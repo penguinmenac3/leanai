e7c473b4b53cdd2335ad86ec7ba890243d6347c4
Michael Fuerst <mail@michaelfuerst.de>
2020-11-10 23:01:54 +0100

    Fixed bugs in example code.

--- deeptech/examples/coco_faster_rcnn.py
+++ deeptech/examples/coco_faster_rcnn.py
@@ -6,6 +6,7 @@ This example shows how to solve COCO using Faster RCNN.
 First we import everything, then we write the config, then we implement the loss and finaly we tell deeptech to run this.
 """
 from collections import namedtuple
+import torch
 from deeptech.data.datasets import COCODataset
 from deeptech.model.module_from_json import Module
 from deeptech.training.trainers import SupervisedTrainer
@@ -23,9 +24,10 @@ class FashionMNISTConfig(Config):
         super().__init__(training_name, data_path, training_results_path)
         # Config of the data
         self.data_dataset = lambda config, split: COCODataset(config, split, COCODataset.InputType, FasterRCNNOutput)
+        self.data_version = 2014
 
         # Config of the model
-        self.model_model = lambda config: Module.create("FasterRCNN", num_classes=config.model_num_classes)
+        self.model_model = lambda config: Module.create_from_file("deeptech/model/models/fasterrcnn.jsonc", "FasterRCNN", num_classes=config.model_num_classes)
         self.model_num_classes = 81
 
         # Config for training
@@ -33,7 +35,10 @@ class FashionMNISTConfig(Config):
         self.training_optimizer = smart_optimizer(SGD)
         self.training_trainer = SupervisedTrainer
         self.training_epochs = 10
-        self.training_batch_size = 32
+        self.training_batch_size = 1
+
+        if torch.cuda.is_available():
+            self.data_device = "cuda"
     
     def create_loss(self, config, model):
         rpn_loss = DetectionLoss(anchors="anchors", pred_boxes="rois/box", pred_class_ids="rois/class_id", target_boxes="boxes", target_class_ids="fg_bg_classes")
