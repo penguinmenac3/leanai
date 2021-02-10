Fixed bug in masking of loss.

deeptech/training/losses/masking.py
@@ -8,12 +8,12 @@ from torch.nn import Module
 
 
 class MaskedLoss(Module):
-    def __init__(self, loss, masking_fun, masked_dim=-1):
+    def __init__(self, loss, masking_fun, keep_dim=1):
         """
         """
         super().__init__()
         self.wrapped_loss = loss
-        self.masked_dim = masked_dim
+        self.keep_dim = keep_dim
         self.masking_fun = masking_fun
 
     def forward(self, y_pred, y_true):
@@ -23,23 +23,31 @@ class MaskedLoss(Module):
         :param y_pred: The predictions of the network. Either a NamedTuple pointing at ITensors or a Dict or Tuple of ITensors.
         :param y_true: The desired outputs of the network (labels). Either a NamedTuple pointing at ITensors or a Dict or Tuple of ITensors.
         """
+        assert len(y_pred.shape) == len(y_true.shape)
+        if self.keep_dim is not None:
+            keep_dim = self.keep_dim if self.keep_dim >= 0 else len(y_pred.shape) + self.keep_dim
+            if keep_dim != len(y_pred.shape) - 1:
+                y_true = torch.transpose(y_true, keep_dim, len(y_true.shape) - 1)
+                y_pred = torch.transpose(y_pred, keep_dim, len(y_pred.shape) - 1)
+        y_pred_N = y_pred.shape[-1]
+        y_true_N = y_true.shape[-1]
+
         binary_mask = self.masking_fun(y_true)
         mask = binary_mask.float()
         masked_y_true = (y_true * mask)[binary_mask]
-        shape = list(y_true.shape)
-        shape[self.masked_dim] = -1
-        masked_y_true = masked_y_true.reshape(shape)
 
         for dim in range(len(binary_mask.shape)):
             if y_pred.shape[dim] != binary_mask.shape[dim] and y_pred.shape[dim] % binary_mask.shape[dim] == 0:
                 repeat = y_pred.shape[dim] / binary_mask.shape[dim]
                 binary_mask = torch.repeat_interleave(binary_mask, int(repeat), dim=dim)
+        mask = binary_mask.float()
         masked_y_pred = (y_pred * mask)[binary_mask]
-        shape = list(y_pred.shape)
-        shape[self.masked_dim] = -1
-        masked_y_pred = masked_y_pred.reshape(shape)
+        
+        if self.keep_dim is not None:
+            masked_y_pred = masked_y_pred.reshape((-1, y_pred_N))
+            masked_y_true = masked_y_true.reshape((-1, y_true_N))
 
-        if masked_y_pred.shape[self.masked_dim] > 0:
+        if masked_y_pred.shape[0] > 0:
             loss = self.wrapped_loss(masked_y_pred, masked_y_true)
         else:
             loss = 0
@@ -48,14 +56,14 @@ class MaskedLoss(Module):
 
 
 class NegMaskedLoss(MaskedLoss):
-    def __init__(self, loss, masked_dim=-1):
+    def __init__(self, loss, keep_dim=1):
         """
         """
-        super().__init__(loss=loss, masking_fun=lambda x: (x >= 0), masked_dim=masked_dim)
+        super().__init__(loss=loss, masking_fun=lambda x: (x >= 0), keep_dim=keep_dim)
 
 
 class NaNMaskedLoss(MaskedLoss):
-    def __init__(self, loss, masked_dim=-1):
+    def __init__(self, loss, keep_dim=1):
         """
         """
-        super().__init__(loss=loss, masking_fun=lambda x: (~x.isnan()), masked_dim=masked_dim)
+        super().__init__(loss=loss, masking_fun=lambda x: (~x.isnan()), keep_dim=keep_dim)