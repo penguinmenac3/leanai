690d3e45174b51fdf62e31737c5fbfbc651dc7c0
Michael Fuerst <mail@michaelfuerst.de>
2021-07-03 10:07:02 +0200

    Added numpy mode to sliced tensors, as this might be required.

--- leanai/core/indexed_tensor_helpers.py
+++ leanai/core/indexed_tensor_helpers.py
@@ -1,4 +1,6 @@
 import torch
+import numpy as np
+
 
 def map_per_batch(fun, values, batch_indices):
     result = []
@@ -6,8 +8,17 @@ def map_per_batch(fun, values, batch_indices):
         result.append(fun(start, stop, value_slice))
     return torch.cat(result)
 
+
 def sliced_per_batch(values, batch_indices):
     slices = torch.where(batch_indices[:-1] - batch_indices[1:] != 0)[0] + 1
+    slices = slices.tolist()
+    slices = zip([0] + slices, slices + [batch_indices.shape[0]])
+    for start, stop in slices:
+        yield start, stop, values[start:stop]
+
+
+def sliced_per_batch_np(values, batch_indices):
+    slices = np.where(batch_indices[:-1] - batch_indices[1:] != 0)[0] + 1
     slices = slices.tolist()
     slices = zip([0] + slices, slices + [batch_indices.shape[0]])
     for start, stop in slices:
