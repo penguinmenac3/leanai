46e14e19dcf4d5bf54b0468f3def6f69ab8e7382
Michael Fuerst <mail@michaelfuerst.de>
2021-12-19 21:38:19 +0100

    Improved doc of config.

--- leanai/core/config.py
+++ leanai/core/config.py
@@ -1,8 +1,45 @@
+"""
+# leanai.core.config
+
+> Make configurations easy and more powerfull.
+
+The DictLike can make configurations easier to use, as it behaves like a dict, but allows for accessing entries like members.
+
+```python
+config = DictLike(foo="bar")
+assert config.foo == config["foo"]
+```
+
+In a config you might want to configure constructable objects, these can be directly called and constructed.
+```python
+config = DictLike(
+    loss=DictLike(
+        type=MyClass,
+        some_param="foo",
+        another_param="bar",
+    )
+)
+config.loss()
+# equivalent
+MyClass(some_param="foo", another_param="bar")
+```
+
+This can be very usefull for configuration of optimizer, dataset and loss,
+which need to be created per node and should be part of the hyperparameters.
+
+Finally you can flatten your config to be a dict where the structure is flattened
+into namespaces separated by underscores (_).
+"""
 from typing import Any
 
 
 class DictLike(dict):
     def __init__(self, **kwargs) -> None:
+        """
+        Create an object that behaves like a dictionary with some extras.
+        
+        You have to specify keys via the keyword arguments.
+        """
         super().__init__(**kwargs)
 
     def __getattribute__(self, __name: str) -> Any:
@@ -15,6 +52,16 @@ class DictLike(dict):
         self[__name] = __value
 
     def flatten(self, depth=0):
+        """
+        Flatten the dictionary hierarchy up to depth (recursive) steps.
+        Keys will be concatenated by underscore to represent the namespaces.
+
+        ```python
+        DictLike(foo=DictLike(bar=42)).flatten() == {"foo_bar": 42}
+        ```
+
+        :param depth: Optional recursion depth for flattening. Defaults to 0, which means no recursion.
+        """
         outp = {}
         for key in self:
             for inner in self[key]:
