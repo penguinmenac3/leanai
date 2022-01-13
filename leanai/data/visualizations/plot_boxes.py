620d4136ce4b1eb3e399f994a2dd1a0a4daa2175
Michael Fuerst <mail@michaelfuerst.de>
2022-01-13 17:53:32 +0100

    Improved box plotting.

--- leanai/data/visualizations/plot_boxes.py
+++ leanai/data/visualizations/plot_boxes.py
@@ -6,7 +6,7 @@
 import cv2
 
 
-def plot_boxes_on_image(image, boxes, color=(0,255,255), thickness=2):
+def plot_boxes_on_image(image, boxes, titles=None, color=(0,255,255), thickness=2):
     """
     Plot the boxes onto the image.
 
@@ -14,10 +14,18 @@ def plot_boxes_on_image(image, boxes, color=(0,255,255), thickness=2):
 
     :param image: The image onto which to draw.
     :param boxes: The boxes which shall be plotted.
+    :param titles: A list of titles to give the boxes (or none).
     :return: An image with the boxes overlayed over the image.
     """
-    for box in boxes:
+    for idx, box in enumerate(boxes):
+        current_color = color
+        if idx in color and isinstance(color[idx], tuple):
+            current_color = color[idx]
         start_point = tuple([int(x) for x in box[:2] - box[2:] // 2])
         end_point = tuple([int(x) for x in box[:2] + box[2:] // 2])
-        image = cv2.rectangle(image, start_point, end_point, color, thickness)
+        image = cv2.rectangle(image, start_point, end_point, current_color, thickness)
+        if titles is not None:
+            org = (max(start_point[0]+2, 2), max(start_point[1] + 22, 22))
+            cv2.putText(img=image, text=str(titles[idx]), org=org,
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.8, color=current_color)
     return image
